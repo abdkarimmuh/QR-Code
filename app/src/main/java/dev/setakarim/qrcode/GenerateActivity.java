d136b7d5f8eb4d655985717e0917e60b9b3c8137
Muhammad Aslam Pangestu Idham <aslampangestu@gmail.com>
2018-09-06 09:07:35 +0700

    Finis qr code. not store yet

--- app/src/main/java/dev/setakarim/qrcode/GenerateActivity.java
+++ app/src/main/java/dev/setakarim/qrcode/GenerateActivity.java
@@ -1,13 +1,90 @@
 package dev.setakarim.qrcode;
 
+import android.graphics.Bitmap;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
 
 public class GenerateActivity extends AppCompatActivity {
+
+    private ImageView imgQRCode;
+    private EditText txtContent;
+    private Button btnGenerate, btnHistory;
+
+    public final static int QRCodeWidth = 500;
+    Bitmap result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_generate);
+
+        txtContent = findViewById(R.id.content_qrcode);
+        imgQRCode = findViewById(R.id.image_qrcode);
+        btnGenerate = findViewById(R.id.generate_qrcode);
+        btnHistory = findViewById(R.id.history);
+
+        btnGenerate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String content = txtContent.getText().toString();
+                try {
+                    result = generate(content);
+                    imgQRCode.setImageBitmap(result);
+                } catch (WriterException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        btnHistory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    private Bitmap generate(String value) throws WriterException{
+        BitMatrix bitMatrix;
+        try {
+            bitMatrix = new MultiFormatWriter().encode(
+                    value,
+                    BarcodeFormat.DATA_MATRIX.QR_CODE,
+                    QRCodeWidth, QRCodeWidth, null
+            );
+
+        } catch (IllegalArgumentException Illegalargumentexception) {
+
+            return null;
+        }
+        int bitMatrixWidth = bitMatrix.getWidth();
+
+        int bitMatrixHeight = bitMatrix.getHeight();
+
+        int[] pixels = new int[bitMatrixWidth * bitMatrixHeight];
+
+        for (int y = 0; y < bitMatrixHeight; y++) {
+            int offset = y * bitMatrixWidth;
+
+            for (int x = 0; x < bitMatrixWidth; x++) {
+
+                pixels[offset + x] = bitMatrix.get(x, y) ?
+                        ContextCompat.getColor(getApplicationContext(),R.color.black):
+                        ContextCompat.getColor(getApplicationContext(),R.color.white);
+            }
+        }
+        Bitmap bitmap = Bitmap.createBitmap(bitMatrixWidth, bitMatrixHeight, Bitmap.Config.ARGB_4444);
+
+        bitmap.setPixels(pixels, 0, 500, 0, 0, bitMatrixWidth, bitMatrixHeight);
+        return bitmap;
     }
 }
---
+++ app/src/main/java/dev/setakarim/qrcode/GenerateHistoryActivity.java
@@ -0,0 +1,8 @@
+package dev.setakarim.qrcode;
+
+/**
+ * Created by mvryan on 06/09/18.
+ */
+
+public class GenerateHistoryActivity {
+}
